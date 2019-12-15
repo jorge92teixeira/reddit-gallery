35be214341e839c0df0208721f407777dc1205d9
Jorge Teixeira <jorgemrt@protonmail.com>
2019-12-15 22:34:55 +0000

    Spinner Component (closes #13)

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,18 +1,14 @@
 /* eslint-disable class-methods-use-this */
 import React from 'react';
 
-import SlideShow from './SlideShow';
+import Spinner from './Spinner';
 
 class Category extends React.Component {
   render() {
     return (
       <div>
         Category Page
-        <SlideShow
-          image={ 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' }
-          close = { () => console.log('close') }
-          arrow = { (a) => console.log(a) }
-        />
+        <Spinner />
       </div>
     );
   }
